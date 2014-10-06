174dc3b20e82a0d91cc69947d08ce28139a4eefc
ZhaoHuan <byzhaohuan@gmail.com>
2014-10-07 00:17:40 +0800

    完成用户关键词生成模块

---
+++ code/CB/generate_user_keywords_by_titlew.py
@@ -0,0 +1,129 @@
+#coding=utf8
+'''
+    从标题分词的结果中进行统计分析，生成每个user的keywords
+    1, 从newsid2title（util中方法生成）中生成news_id和title（分词后）的映射
+    2, 从训练数据中获取用户的阅读列表uid-newsids
+    3, 利用1中生成的map获取用户阅读的全部关键词
+    4，对关键词进行统计，输出topN by frequency
+'''
+from util import extract_title
+
+data_dir = '/Users/huanzhao/projects/recommendation-system-contest/data/'
+train_data_path = data_dir + 'train_data.txt'
+topN = 5
+user_topkeywords_path = data_dir + 'uid2top%skeywords.txt' % topN
+
+def generate_newsid2title_map():
+    '''
+        完成step1
+    '''
+    print 'run generate_newsid2title_map...'
+    newsid2title_filename = data_dir + 'newsid_title_map.txt'
+    lines = open(newsid2title_filename, 'r').readlines()
+    newsid2title_map = {}
+    for l in lines:
+        parts = l.strip().split('\t')
+        if parts[0] in newsid2title_map:
+            continue
+        title_segments = parts[2].split('-')
+        newsid2title_map[parts[0]] = title_segments
+    return newsid2title_map
+
+def generate_uid2newsids_map():
+    '''
+        完成step2
+    '''
+    print 'run generate_uid2newsids_map...'
+    uid2newsids_map = {}
+    f = open(train_data_path, 'r')
+    line = f.readline()
+    while line:
+        parts = line.strip().split('\t')
+        uid2newsids_map.setdefault(parts[0], []).append(parts[1])
+        line = f.readline()
+    return uid2newsids_map
+
+def generate_user_keywords(uid2newsids_map, newsid2title_map):
+    '''
+        完成step3
+    '''
+    print 'run generate_user_keywords...'
+    user_keywords = []
+    for uid, newsids in uid2newsids_map.items():
+        keywords = []
+        for nid in newsids:
+            keywords.extend(newsid2title_map.get(nid, []))
+        user_keywords.append((uid, keywords))
+    return user_keywords
+
+def generate_top_keywords(user_keywords):
+    '''
+        完成step4
+        结果存在文件中
+    '''
+    print 'run generate_top_keywords...'
+    def get_keywords_cnt(keywords):
+        '''
+            传入一个keywords列表，统计次数，按照cnt降序返回(keyword, cnt)的list
+        '''
+        words_cnt = {}
+        for kwd in keywords:
+            words_cnt[kwd] = words_cnt.get(kwd, 0) + 1
+        keywords_cnt = sorted(words_cnt.items(), key=lambda d: d[1], reverse=True)
+        return keywords_cnt
+
+    user_topkeywords = []
+    for uid, keywords in user_keywords:
+        #import pdb;pdb.set_trace()
+        keywords_cnt = get_keywords_cnt(keywords)
+        user_topkeywords.append((uid, keywords_cnt))
+        #line = uid + '\t' + ','.join([kwd+'-'+str(cnt) for kwd, cnt in keywords_cnt[:topN]])
+        #f.write(line + '\n')
+    #keywords的形式 (kwd, cnt)，这里排序是希望最后的user-topkeywords的结果
+    #按照top 1的word的cnt降序排列，便于观察数据
+    user_topkeywords= sorted(user_topkeywords, key=lambda d: d[1][0][1], reverse=True)
+
+    f = open(user_topkeywords_path, 'w+')
+    for uid, keywords_cnt in user_topkeywords:
+        line = uid + '\t' + ','.join([kwd+'-'+str(cnt) for kwd, cnt in keywords_cnt[:topN]])
+        f.write(line + '\n')
+    f.close()
+
+def generate_user_topkeywords():
+    print 'run generate_user_top%skeywords, consist of 4 steps...\n' % topN
+
+    newsid2title_map = generate_newsid2title_map()
+    print_map(newsid2title_map)
+
+    uid2newsids_map = generate_uid2newsids_map()
+    print_map(uid2newsids_map)
+
+    user_keywords = generate_user_keywords(uid2newsids_map, newsid2title_map)
+    #print user_keywords[:5]
+
+    generate_top_keywords(user_keywords)
+
+    print '\nfinished, res saved in %s' % user_topkeywords_path
+
+def print_map(map_, limit=5):
+    info = []
+    for k, w in map_.items():
+        if isinstance(w, list):
+            w = '-'.join(w)
+        unit = '(%s, %s)' % (k, w)
+        info.append(unit)
+        if len(info) > limit:
+            break
+    print ','.join(info)
+
+def main():
+    generate_user_topkeywords()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+

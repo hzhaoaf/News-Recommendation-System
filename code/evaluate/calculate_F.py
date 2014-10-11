84a06a227d884948308a75aaf0a0ca1f82435788
ZhaoHuan <byzhaohuan@gmail.com>
2014-10-11 16:17:24 +0800

    改进calculate_F.py: 通过运行命令行和输入文件即可得到评测结果

--- code/evaluate/calculate_F.py
+++ code/evaluate/calculate_F.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+'''
+    这个模块对推荐结果进行评测，计算F值；
+    输入: recommend_res_file, golden_test
+    输出: (precision, recall, F)
+'''
+import sys
 
 __author__ = 'wangjz'
 
@@ -12,7 +18,7 @@ def calculate_F(recommend, reality):
     (107,100647132),
     ……]
 
-    【reality】 userid,newsid 一对一
+    【reality】为set类型， userid,newsid 一对一
     [(35,100647112),
     (107,100647112),
     (111,100647112),]
@@ -27,7 +33,7 @@ def calculate_F(recommend, reality):
     sum_hitu = 0
 
     for item in recommend:
-        if item in reality: #疑问，这样的查询效率怎样？
+        if item in reality: #疑问，这样的查询效率怎样？ 回答: 将reality的类型改为set即可
             sum_hitu += 1
 
     precision = 1.0 * sum_hitu / sum_Lu
@@ -36,17 +42,39 @@ def calculate_F(recommend, reality):
 
     return (precision, recall,F)
 
+def test():
+    rec = [(1,2),
+           (1,3),
+           [1,4],
+           [2,1],
+           [2,2],
+           [3,2]]
 
-rec = [(1,2),
-       (1,3),
-       [1,4],
-       [2,1],
-       [2,2],
-       [3,2]]
+    rel = [(1,1),
+           [2,2],
+           [3,2]]
 
-rel = [(1,1),
-       [2,2],
-       [3,2]]
+    ans = calculate_F(rec,rel)
+    print ans
 
-ans = calculate_F(rec,rel)
-print ans
+def run(recommend_res_file, golden_test_file):
+    '''
+        读入recommend_res_file, golden_test_file两个文件，生成计算F值需要的格式
+    '''
+
+    def load_items(items_file):
+        lines = open(items_file, 'r').readlines()
+        return [l.strip() for l in lines if not l.startswith('#')]
+
+    rec_items = load_items(recommend_res_file)
+    gt_items = set(load_items(golden_test_file))
+    print 'res is(Precision, Recall, F)\n', calculate_F(rec_items, gt_items)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print 'please input valid parameters, valid example is:\n python calculate_F.py recommmend_res_file golden_test_file'
+        sys.exit(0)
+    recommend_res_file = sys.argv[1]
+    golden_test_file = sys.argv[2]
+    run(recommend_res_file, golden_test_file)
+
